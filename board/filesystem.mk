ac36465796868d32fd63845c01dd3a21c8e4953e
pinnamanivenkat <pinnamanivenkat@gmail.com>
2016-09-29 20:28:58 +0530

    Fix for twrp theme

--- board/filesystem.mk
+++ board/filesystem.mk
@@ -15,3 +15,41 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.mt6735
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
+
+#If tou get Error as TW_Theme() not found
+# comment lines below #Recovery and uncomment these lines
+#TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+#DEVICE_RESOLUTION := 1080x1920
+#DEVICE_SCREEN_WIDTH := 1080
+#DEVICE_SCREEN_HEIGHT := 1920
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+#TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+#BOARD_HAS_NO_SELECT_BUTTON := true
+#TW_NO_REBOOT_BOOTLOADER := true
+#TW_THEME := portrait_hdpi
+#TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+#TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+#TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+#RECOVERY_SDCARD_ON_DATA := true
+#TW_INTERNAL_STORAGE_PATH := "/data/media"
+#TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+#TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+#TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+#TW_DEFAULT_EXTERNAL_STORAGE := true
+#TW_MAX_BRIGHTNESS := 255
+#BOARD_SUPPRESS_SECURE_ERASE := true
+#TW_INCLUDE_CRYPTO := true
+#TW_CRYPTO_FS_TYPE := "ext4"
+#TW_CRYPTO_MNT_POINT := "/data"
+#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data =ordered"
+#TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+#TW_MAX_BRIGHTNESS := 255
+#TW_NO_USB_STORAGE := true
+#BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
+#TARGET_DISABLE_TRIPLE_BUFFERING := false
+#TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+#TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.mt6735
+#BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_HAS_LARGE_FILESYSTEM := true
+
