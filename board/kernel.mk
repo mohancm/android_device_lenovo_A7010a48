84420ee067242733d855c1b78429c8ec962e20df
mohancm100 <mohanmanjappa@gmail.com>
2016-11-05 21:45:23 +0530

    changes made

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,8 +1,8 @@
 # Kernel informations
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
-BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --board 1465391499 --ramdisk_offset 0x04f88000 --second_offset 0x00e88000 --tags_offset 0x03f88000 --board mohan
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive androidboot.selinux=disabled
+BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --board 1465391499 --ramdisk_offset 0x03f88000 --second_offset 0x40f0000 --tags_offset 0x0df88000 --board mohan
 
 # Kernel properties
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
