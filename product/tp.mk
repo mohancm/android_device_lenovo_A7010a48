3558c9bdeda10864c495a4247ac6a0ece8b2b93f
mohancm100 <mohanmanjappa@gmail.com>
2016-11-12 01:30:55 +0530

    thermal files needed to bootup

---
+++ product/tp.mk
@@ -0,0 +1,8 @@
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/tp/etc/.tp/.ht120.mtc:system/etc/.tp/.ht120.mtc \
+	$(LOCAL_PATH)/tp/etc/.tp/.thermal_policy_00:system/etc/.tp/.thermal_policy_00 \
+	$(LOCAL_PATH)/tp/etc/.tp/thermal.conf:system/etc/.tp/thermal.conf \
+	$(LOCAL_PATH)/tp/etc/.tp/thermal.off.conf:system/etc/.tp/thermal.off.conf \
+	$(LOCAL_PATH)/tp/bin/thermal:system/bin/thermal \
+	$(LOCAL_PATH)/tp/bin/thermal_manager:system/bin/thermal_manager \
+	$(LOCAL_PATH)/tp/bin/thermald:system/bin/thermald 
