db9688a18fd06182f315fba0e33c330c48907e13
pinnamanivenkat <pinnamanivenkat@gmail.com>
2016-11-29 21:52:09 +0530

    To build shim package

--- device_A7010a48.mk
+++ device_A7010a48.mk
@@ -21,6 +21,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Device specific overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+      libgui_shim
+
 # Device product elements
 include $(LOCAL_PATH)/product/*.mk
 
