b3e65d0dfc244fb1bcb72a04306b2cb1efb564f4
mohancm100 <mohanmanjappa@gmail.com>
2016-12-04 02:50:00 +0530

    audio Fixes

--- product/audio.mk
+++ product/audio.mk
@@ -9,3 +9,8 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libtinyxml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_device.xml:system/etc/audio_device.xml \
+    $(LOCAL_PATH)/audio/audio_em.xml:system/etc/audio_em.xml \
+	$(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf
