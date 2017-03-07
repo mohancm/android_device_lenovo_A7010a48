c94d14f2f6fafa08c47cbf9a960c435c5dd9ca8d
mohancm100 <mohanmanjappa@gmail.com>
2017-03-07 23:14:55 +0530

    Nougut wifi hals

--- product/wifi.mk
+++ product/wifi.mk
@@ -3,12 +3,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
+# WiFi
 PRODUCT_PACKAGES += \
-    lib_driver_cmd_mt66xx \
-    libwifi-hal-mt66xx \
-    libwpa_client \
+    dhcpcd.conf \
     hostapd \
-    wpa_supplicant
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+	PRODUCT_PACKAGES += \
+    lib_driver_cmd_mt66xx
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
