981660dea3a0b149e6db93c6e7900838ae5ef865
mohancm100 <mohanmanjappa@gmail.com>
2017-03-18 20:41:22 +0530

    Remove unused patches

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av frameworks/base frameworks/native hardware/libhardware packages/apps/Settings system/core system/netd"
+dirs="bionic frameworks/av packages/apps/Settings system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
@@ -11,6 +11,12 @@ for dir in $dirs ; do
 	git apply $rootdirectory/device/lenovo/A7010a48/patches/$dir/*.patch
 	echo " "
 done
+
+#cd $rootdirectory
+#cd packages/apps/SetupWizard
+#echo "Applying packages/apps/SetupWizard patches..."
+#git reset cd34a5865fc0799ddd222b9871b4fd3f08b9d5cf --hard
+#echo " "
 
 # -----------------------------------
 echo "Changing to build directory..."
