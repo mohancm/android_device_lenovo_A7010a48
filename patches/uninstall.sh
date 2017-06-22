b2685a907b3afef5982ba25ad68b6ece8cba0ef7
mohancm100 <mohanmanjappa@gmail.com>
2017-06-22 21:34:13 +0530

    Update Patches
    Signed-off-by: mohancm100 <mohanmanjappa@gmail.com>

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic system/sepolicy frameworks/av frameworks/base frameworks/native frameworks/opt/telephony frameworks/opt/net/ims hardware/libhardware packages/apps/Settings packages/services/Telecomm packages/services/Telephony system/netd system/core system/bt"
+dirs="bionic frameworks/av packages/apps/Settings system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
