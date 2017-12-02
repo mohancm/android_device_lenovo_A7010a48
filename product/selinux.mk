ede18cbcaf67766acf3f8c3f7cf6d202833f3958
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-12-02 19:14:02 +0530

    Update seccomp policies

--- product/selinux.mk
+++ product/selinux.mk
@@ -1,7 +1,7 @@
 # SELinux
 BOARD_SEPOLICY_DIRS := \
-       $(LOCAL_PATH)/sepolicy/basic
-       $(LOCAL_PATH)/sepolicy/bsp
+       $(LOCAL_PATH)/sepolicy/basic \
+       $(LOCAL_PATH)/sepolicy/bsp \
        $(LOCAL_PATH)/sepolicy/full
        
 # Seccomp Filter
