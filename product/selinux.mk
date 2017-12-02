247178ffcbba2a9ed4dc4fb7ead1974580e150de
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-12-02 19:04:56 +0530

    Add MTK O sepolicies

--- product/selinux.mk
+++ product/selinux.mk
@@ -1,7 +1,9 @@
 # SELinux
 BOARD_SEPOLICY_DIRS := \
-       $(LOCAL_PATH)/sepolicy
-
+       $(LOCAL_PATH)/sepolicy/basic
+       $(LOCAL_PATH)/sepolicy/bsp
+       $(LOCAL_PATH)/sepolicy/full
+       
 # Seccomp Filter
 BOARD_SECCOMP_POLICY := \
        $(LOCAL_PATH)/seccomp
